56775484f7f455260b3ac3b226c86702768467a2
daniepusb <danie.p.usb@gmail.com>
2019-12-19 20:16:06 -0300

    first commit

---
+++ app/src/main/java/danielpedroza/com/pruebacondiego/MainActivity.java
@@ -0,0 +1,23 @@
+package danielpedroza.com.pruebacondiego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+    }
+
+}
